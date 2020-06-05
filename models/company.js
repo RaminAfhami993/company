663bafeed72c7c5854363d797b6e722d5f8e907e
unknown <afhami.ramin@yahoo.com>
2020-06-05 15:42:21 +0430

    add company model

---
+++ models/company.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const CompanySchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+});
+
+
+module.exports = mongoose.model('Company', CompanySchema);
+
+
+
